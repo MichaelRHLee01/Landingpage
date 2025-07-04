07fc06116f6edc2bd18145592f68171a567b10aa
Michael Lee <raehyunl@andrew.cmu.edu>
2025-07-04 20:30:06 +0900

    api.js updated with react app url

--- landingpage/src/api.js
+++ landingpage/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api',
     timeout: 600000, // 10 second timeout
     headers: {
         'Content-Type': 'application/json',
